f89a6cedbcb4f818e0ccf08e5395ba54d40ec362
vermajnv <nayanrahul.jnv@gmail.com>
2018-06-19 14:42:31 +0530

    js Fundamental Initail POC_V1

---
+++ poc.v1/app.js
@@ -0,0 +1,9 @@
+function doSomthing() {
+    console.log("Doing something..");
+    document.querySelector('#nameText').value;
+    document.querySelector('#addressText').value;
+
+}
+
+document.querySelector('#submit').addEventListener('click', doSomthing);
+
